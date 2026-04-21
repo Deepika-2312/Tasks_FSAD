eb9f6f7831ba36df61609cbefee537cd3f4fd2c4
Vtu24451 <vtu24451@veltech.edu.in>
2026-04-21 09:08:55 +0530

    Add files via upload

---
+++ example-1/src/main/java/com/example/example_1/Example1Application.java
@@ -0,0 +1,17 @@
+package com.example.example_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example1Application.class, args);
+		printer();
+	}
+	public static void printer() {
+		System.out.println("Hello World..!");
+	}
+
+}
